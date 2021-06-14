33ce3eb3fb1e4d8481e1153aae25c7113432eea6
xiabingp <986001836@qq.com>
2021-06-14 17:06:16 +0800

    while

---
+++ jump7.py
@@ -0,0 +1,13 @@
+'''a = 0
+while a < 10:
+    if a == 5:
+        continue
+    a=a+1
+    print(a)'''
+# continue的用法
+b = 0
+while b < 10:
+    b = b + 1
+    if b == 5:
+        continue
+    print(b)
